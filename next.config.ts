5f4d00efd10f547de952cd81a92b562723272da7
Anmol <kenkani0408@gmail.com>
2026-01-06 13:27:59 +0530

    Unresolve conflict merged

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
   crossOrigin: `anonymous`
-=======
-  crossOrigin: 'anonymous'
->>>>>>> e71d9498a0ffdf18f6a5c3f0d08a74f18e19cc27
 };
 
 export default nextConfig;
